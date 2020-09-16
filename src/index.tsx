fd64b64a80e9b39f6a8308ac3178e4d288afed17
lsh58 <tmdgusdl58@gmail.com>
2020-09-16 11:23:08 +0900

    create-react-app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CounterStore from './stores/counter'; // 방금 만든 스토어 불러와줍니다.
+
+const counter = new CounterStore(); // 스토어 인스턴스를 만들고
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider counter={counter}>
+    {/* Provider 에 props 로 넣어줍니다. */}
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
