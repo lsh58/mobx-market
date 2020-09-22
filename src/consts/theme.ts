81354fec95ab98ed7479fcec79fd62fc0b814860
lsh58 <tmdgusdl58@gmail.com>
2020-09-22 18:54:23 +0900

    mui theme 적용ing

---
+++ src/consts/theme.ts
@@ -0,0 +1,59 @@
+import {
+  createMuiTheme,
+} from '@material-ui/core'
+import * as colors from '@material-ui/core/colors'
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme'
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    primary: colors.blue,
+    secondary: colors.pink,
+    background: {
+      default: colors.grey[200]
+    }
+  },
+  typography: {
+    fontFamily: '"Roboto", "Noto sans KR", sans-serif',
+    h1: {
+      fontSize: '1.625rem'
+    },
+    h2: {
+      fontSize: '1.5rem',
+      fontWeight: 500
+    },
+    h3: {
+      fontSize: '1.375rem'
+    },
+    h4: {
+      fontSize: '1.25rem'
+    },
+    h5: {
+      fontSize: '1.125rem'
+    },
+    h6: {
+      fontSize: '1.125rem',
+      fontWeight: 400
+    }
+  },
+  overrides: {
+    MuiMenuItem: {
+      root: {
+        fontSize: '0.875rem'
+      }
+    },
+    MuiInputLabel: {
+      shrink: {
+        fontSize: '1.125rem'
+      }
+    },
+    MuiTooltip: {
+      tooltip: {
+        color: colors.blue[600],
+        backgroundColor: 'white',
+        border: `1px solid ${colors.blue[600]}`,
+        fontSize: '1rem'
+      }
+    }
+  }
+}
+export default createMuiTheme(themeOptions)
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import App from './App';
 import MarketStore from './stores/market';
+import { ThemeProvider } from "@material-ui/styles";
+import theme from './consts/theme';
 
 const market = new MarketStore();
 
 ReactDOM.render(
   <Provider market={market}>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
